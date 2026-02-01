97d3a36042391ae17531a89f77500d446579b319
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-02-01 18:53:47 -0300

    feat: base structure for response of root route of application added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', function (Request $request) {
+
+    return response()->json([
+        'status' => 200,
+        'online_time' => null,
+        'memory_use' => memory_get_usage(true),
+        'last_datetime_executed_cron' => now(),
+    ], 200);
+    
+});
