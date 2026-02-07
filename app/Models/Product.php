4bed240be670dc432d2e798bf3b25ab4edf2741c
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-02-07 06:42:17 -0300

    fix: fields added to fillable property on Product Model and primaryKey changed to code, besides timestamp set up to false

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $primaryKey = 'code';
+    protected $fillable = ['code', 'product_name', 'quantity', 'url', 'creator', 'created_t', 'created_datetime', 'imported_t', 'status'];
+    public $timestamps = false;
+
     use HasFactory;
 }
