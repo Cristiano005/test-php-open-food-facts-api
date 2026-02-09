85b68a19cba79e02578cfb98c8902d556f6078a0
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-02-09 01:55:16 -0300

    feat: system data added to root route

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,27 @@
 <?php
 
 use App\Http\Controllers\Api\V1\ProductController;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (Request $request) {
+Route::get('/', function () {
+
+    $isConnectionOk = true;
+
+    try {
+        DB::connection()->getPdo();
+        DB::table('products')->select('code')->get();
+    } catch (PDOException) {
+        $isConnectionOk = false;
+    }
 
     return response()->json([
-        'status' => 200,
-        'online_time' => null,
-        'memory_use' => memory_get_usage(true),
-        'last_datetime_executed_cron' => now(),
+        'database_connection' => $isConnectionOk ? 'ok' : 'failed',
+        'online_time' => now()->subSeconds(floatval(file_get_contents('/proc/uptime')))->diffForHumans(),
+        'memory_use' => (memory_get_usage(true) / 1024) / 1024 . "MB",
+        'last_cron_updated' => Cache::get('last-cron-updated', 'No updates'),
     ], 200);
-
 });
 
 Route::get('/products', [ProductController::class, 'index']);
