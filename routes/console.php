a4bbad7ae98977a4dccc1bc269529a4859f2aaa3
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-02-09 01:53:04 -0300

    feat: added enviroment variable for define time to execute cron everyday

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use App\Console\Commands\ImportProductsCommand;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(ImportProductsCommand::class)->dailyAt('06:21');
+Schedule::command(ImportProductsCommand::class)->dailyAt(env('CRON_RUN_TIME', '00:00'));
